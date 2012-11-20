414b9a01768b446e0a344b0cd80e986ef81b7625
Jeremy Cole <jeremy@jcole.us>
2012-11-20 11:38:44 -0800

    Make Cursor use a stack; Add support for two integer types

--- lib/innodb/cursor.rb
+++ lib/innodb/cursor.rb
@@ -3,7 +3,7 @@ class Innodb::Cursor
 
   def initialize(buffer, offset)
     @buffer = buffer
-    @cursor = offset
+    @cursor = [ offset ]
     @direction = :forward
   end
 
@@ -17,26 +17,53 @@ class Innodb::Cursor
     self
   end
 
+  def position
+    @cursor[0]
+  end
+
   def seek(offset)
-    @cursor = offset if offset
-    @cursor
+    @cursor[0] = offset if offset
+    self
   end
 
   def adjust(relative_offset)
-    @cursor += relative_offset
-    @cursor
+    @cursor[0] += relative_offset
+    self
+  end
+
+  def push(offset=nil)
+    @cursor.unshift(offset.nil? ? @cursor[0] : offset)
+    self
+  end
+
+  def pop
+    @cursor.shift
+    self
+  end
+
+  # Execute a block and restore the cursor to the previous position after
+  # the block returns. Return the block's return value after restoring the
+  # cursor.
+  def peek
+    raise "No block given" unless block_given?
+    push
+    result = yield
+    pop
+    result
   end
 
   def read_and_advance(length, type=nil)
     data = nil
+    #print "data(#{@cursor[0]}..."
     case @direction
     when :forward
-      data = @buffer.data(@cursor, length)
+      data = @buffer.data(@cursor[0], length)
       adjust(length)
     when :backward
       adjust(-length)
-      data = @buffer.data(@cursor, length)
+      data = @buffer.data(@cursor[0], length)
     end
+    #puts "#{@cursor[0]}) = #{data.bytes.map { |n| "%02x" % n }.join}"
     type ? data.unpack(type).first : data
   end
 
@@ -60,6 +87,14 @@ class Innodb::Cursor
     (uint & 32768) == 0 ? uint : -(uint ^ 65535) - 1
   end
 
+  def get_uint24(offset=nil)
+    seek(offset)
+    # Ruby 1.8 doesn't support big-endian 24-bit unpack; unpack as one
+    # 8-bit and one 16-bit big-endian instead.
+    high, low = read_and_advance(3).unpack("nC")
+    (high << 8) | low
+  end
+
   def get_uint32(offset=nil)
     seek(offset)
     read_and_advance(4, "N")
@@ -72,4 +107,26 @@ class Innodb::Cursor
     high, low = read_and_advance(8).unpack("NN")
     (high << 32) | low
   end
+
+  # InnoDB compressed 32-bit unsigned integer.
+  def get_ic_uint32
+    flag = peek { get_uint8 }
+
+    case
+    when flag < 0x80
+      get_uint8
+    when flag < 0xc0
+      get_uint16 & 0x7fff
+    when flag < 0xe0
+      get_uint24 & 0x3fffff
+    when flag < 0xf0
+      get_uint32 & 0x1fffffff
+    when flag == 0xf0
+      adjust(+1) # Skip the flag.
+      get_uint32
+    else
+      raise "Invalid flag #{flag.to_s(16)} seen"
+    end
+  end
+
 end
