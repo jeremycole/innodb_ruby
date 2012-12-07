cd5d2b73215d515097f46eb72634ac4823b1c9f3
Jeremy Cole <jeremy@jcole.us>
2012-12-07 12:45:40 -0800

    Added comments to index search functions

--- lib/innodb/index.rb
+++ lib/innodb/index.rb
@@ -122,12 +122,21 @@ class Innodb::Index
   def linear_search_from_cursor(cursor, key)
     this_rec = cursor.record
 
+    # Iterate through all records until finding either a matching record or
+    # one whose key is greater than the desired key.
     while this_rec && next_rec = cursor.record
+      # If we reach supremum, return the last non-system record we got.
       return this_rec if next_rec[:header][:type] == :supremum
+
       if (compare_key(key, this_rec[:key]) >= 0) &&
         (compare_key(key, next_rec[:key]) < 0)
+        # The desired key is either an exact match for this_rec or is greater
+        # than it but less than next_rec. If this is a non-leaf page, that
+        # will mean that the record will fall on the leaf page this node
+        # pointer record points to, if it exists at all.
         return this_rec
       end
+
       this_rec = next_rec
     end
 
@@ -140,29 +149,54 @@ class Innodb::Index
   # linear search using linear_search_from_cursor to find the closest record
   # whose key is not greater than the desired key. (If an exact match is
   # desired, the returned record must be checked in the same way as the above
-  # linear_search_from_cursor function.
+  # linear_search_from_cursor function.)
   def binary_search_by_directory(page, dir, key)
     return nil if dir.empty?
 
+    # Split the directory at the mid-point (using integer math, so the division
+    # is rounding down). Retrieve the record that sits at the mid-point.
     mid = dir.size / 2
     rec = page.record(dir[mid])
 
+    # The mid-point record was the infimum record, which is not comparable with
+    # compare_key, so we need to just linear scan from here. If the mid-point
+    # is the beginning of the page there can't be many records left to check
+    # anyway.
     if rec[:header][:type] == :infimum
       return linear_search_from_cursor(page.record_cursor(rec[:next]), key)
     end
 
+    # Compare the desired key to the mid-point record's key.
     case compare_key(key, rec[:key])
     when 0
+      # An exact match for the key was found. Return the record.
       rec
     when +1
-      if mid == 0
+      # The mid-point record's key is less than the desired key.
+      if dir.size == 1
+        # This is the last entry remaining from the directory, use linear
+        # search to find the record. We already know that there wasn't an
+        # exact match, so skip the current record and start cursoring from
+        # the next record.
         linear_search_from_cursor(page.record_cursor(rec[:next]), key)
       else
+        # There are more entries remaining from the directory, recurse again
+        # using binary search on the right half of the directory, which
+        # represents values greater than or equal to the mid-point record's
+        # key.
         binary_search_by_directory(page, dir[mid...dir.size], key)
       end
     when -1
-      return nil if mid == 0
-      binary_search_by_directory(page, dir[0...mid], key)
+      # The mid-point record's key is greater than the desired key.
+      if dir.size == 1
+        # If this is the last entry remaining from the directory, we didn't
+        # find anything workable.
+        nil
+      else
+        # Recurse on the left half of the directory, which represents values
+        # less than the mid-point record's key.
+        binary_search_by_directory(page, dir[0...mid], key)
+      end
     end
   end
 
@@ -177,8 +211,13 @@ class Innodb::Index
     while rec =
       linear_search_from_cursor(page.record_cursor(page.infimum[:next]), key)
       if page.level > 0
+        # If we haven't reached a leaf page yet, move down the tree and search
+        # again using linear search.
         page = @space.page(rec[:child_page_number])
       else
+        # We're on a leaf page, so return the page and record if there is a
+        # match. If there is no match, break the loop and cause nil to be
+        # returned.
         return page, rec if compare_key(key, rec[:key]) == 0
         break
       end
@@ -193,8 +232,13 @@ class Innodb::Index
 
     while rec = binary_search_by_directory(page, page.directory.dup, key)
       if page.level > 0
+        # If we haven't reached a leaf page yet, move down the tree and search
+        # again using binary search.
         page = @space.page(rec[:child_page_number])
       else
+        # We're on a leaf page, so return the page and record if there is a
+        # match. If there is no match, break the loop and cause nil to be
+        # returned.
         return page, rec if compare_key(key, rec[:key]) == 0
         break
       end
