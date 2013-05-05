291ab135a62a58d89230630ff87db1ab27569c48
Jeremy Cole <jeremy@jcole.us>
2013-05-05 03:02:49 -0700

    Add additional tests for Innodb::Xdes

--- spec/innodb/xdes_spec.rb
+++ spec/innodb/xdes_spec.rb
@@ -33,22 +33,45 @@ describe Innodb::Xdes do
 
   describe "#read_xdes_entry" do
     it "calculates the start_page correctly" do
-      @xdes0.xdes[:start_page].should eql 0
-      @xdes1.xdes[:start_page].should eql 64
+      @xdes0.start_page.should eql 0
+      @xdes1.start_page.should eql 64
     end
 
-    it "has the right keys and values" do
-      @xdes0.xdes[:start_page].should eql 0
-      @xdes0.xdes[:fseg_id].should eql 0
-      @xdes0.xdes[:this].should be_an_instance_of Hash
-      @xdes0.xdes[:list].should be_an_instance_of Hash
-      @xdes0.xdes[:bitmap].size.should eql 16
+    it "calculates the end_page correctly" do
+      @xdes0.end_page.should eql 63
+      @xdes1.end_page.should eql 127
+    end
+
+    it "has the right methods and values" do
+      @xdes0.start_page.should eql 0
+      @xdes0.fseg_id.should eql 0
+      @xdes0.this.should be_an_instance_of Hash
+      @xdes0.list.should be_an_instance_of Hash
+      @xdes0.bitmap.size.should eql 16
     end
   end
 
   describe "#xdes" do
     it "is a Hash" do
       @xdes0.xdes.should be_an_instance_of Hash
+    end
+  end
+
+  describe "#allocated_to_fseg?" do
+    it "works correctly" do
+      @space.xdes_for_page(0).allocated_to_fseg?.should eql false
+      @space.xdes_for_page(64).allocated_to_fseg?.should eql true
+    end
+  end
+
+  describe "#page_status" do
+    it "returns the status of a page" do
+      status = @xdes0.page_status(0)
+      status.should be_an_instance_of Hash
+      status.size.should eql 3
+      status[:page].should eql 0
+      status[:free].should eql false
+      status[:clean].should eql true
     end
   end
 
@@ -79,6 +102,13 @@ describe Innodb::Xdes do
   describe "#used_pages" do
     it "returns the number of used pages" do
       @xdes0.used_pages.should eql 64
+    end
+  end
+
+  describe "#==" do
+    it "compares by page and offset" do
+      (@xdes0 == @xdes1).should eql false
+      (@xdes0 == @space.xdes_for_page(0)).should eql true
     end
   end
 end
