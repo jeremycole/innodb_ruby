e850493c7be546d806833820ae1ea60d99c99d33
Jeremy Cole <jeremy@jcole.us>
2013-05-31 19:11:41 -0700

    Add support for BLOB pages

---
+++ lib/innodb/page/blob.rb
@@ -0,0 +1,60 @@
+# -*- encoding : utf-8 -*-
+
+class Innodb::Page::Blob < Innodb::Page
+  def pos_blob_header
+    pos_fil_header + size_fil_header
+  end
+
+  def size_blob_header
+    4 + 4
+  end
+
+  def pos_blob_data
+    pos_blob_header + size_blob_header
+  end
+
+  def blob_header
+    cursor(pos_blob_header).name("blob_header") do |c|
+      {
+        :length => c.name("length") { c.get_uint32 },
+        :next   => c.name("next") { Innodb::Page.maybe_undefined(c.get_uint32) },
+      }
+    end
+  end
+
+  def blob_data
+    cursor(pos_blob_data).name("blob_data") do |c|
+      c.get_bytes(blob_header[:length])
+    end
+  end
+
+  def dump_hex(string)
+    slice_size = 16
+    bytes = string.split("").map { |s| s.ord }
+    string.split("").each_slice(slice_size).each_with_index do |slice_bytes, slice_count|
+      puts "%08i  %-23s  %-23s  |%-16s|" % [
+        (slice_count * slice_size),
+        slice_bytes[0..8].map { |n| "%02x" % n.ord }.join(" "),
+        slice_bytes[8..16].map { |n| "%02x" % n.ord }.join(" "),
+        slice_bytes.join(""),
+      ]
+    end
+  end
+
+  # Dump the contents of a page for debugging purposes.
+  def dump
+    super
+
+    puts "blob header:"
+    pp blob_header
+    puts
+
+    puts "blob data:"
+    dump_hex(blob_data)
+    puts
+
+    puts
+  end
+end
+
+Innodb::Page::SPECIALIZED_CLASSES[:BLOB] = Innodb::Page::Blob
