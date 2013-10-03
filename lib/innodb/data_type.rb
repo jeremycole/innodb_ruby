9ff91f9b29d9a5d2a4c92c89f5ee0b49049817c3
Davi Arnaut <davi@verdesmares.com>
2013-10-03 15:24:03 -0700

    Add support for numeric types: BIT, FLOAT, DOUBLE and DECIMAL.

--- lib/innodb/data_type.rb
+++ lib/innodb/data_type.rb
@@ -1,5 +1,24 @@
 # -*- encoding : utf-8 -*-
+require "stringio"
+require "bigdecimal"
+
 class Innodb::DataType
+
+  # MySQL's Bit-Value Type (BIT).
+  class BitType
+    attr_reader :name, :width
+
+    def initialize(base_type, modifiers, properties)
+      nbits = modifiers.fetch(0, 1)
+      raise "Unsupported width for BIT type." unless nbits >= 0 and nbits <= 64
+      @width = (nbits + 7) / 8
+      @name = Innodb::DataType.make_name(base_type, modifiers, properties)
+    end
+
+    def value(data)
+      "0b%b" % BinData::const_get("Uint%dbe" % (@width * 8)).read(data)
+    end
+  end
 
   class IntegerType
     attr_reader :name, :width
@@ -12,6 +31,8 @@ class Innodb::DataType
 
     def base_type_width_map
       {
+        :BOOL       => 1,
+        :BOOLEAN    => 1,
         :TINYINT    => 1,
         :SMALLINT   => 2,
         :MEDIUMINT  => 3,
@@ -32,6 +53,121 @@ class Innodb::DataType
 
     def get_int(data, nbits)
       BinData::const_get("Int%dbe" % nbits).read(data) ^ (-1 << (nbits - 1))
+    end
+  end
+
+  class FloatType
+    attr_reader :name, :width
+
+    def initialize(base_type, modifiers, properties)
+      @width = 4
+      @name = Innodb::DataType.make_name(base_type, modifiers, properties)
+    end
+
+    # Read a little-endian single-precision floating-point number.
+    def value(data)
+      BinData::FloatLe.read(data)
+    end
+  end
+
+  class DoubleType
+    attr_reader :name, :width
+
+    def initialize(base_type, modifiers, properties)
+      @width = 8
+      @name = Innodb::DataType.make_name(base_type, modifiers, properties)
+    end
+
+    # Read a little-endian double-precision floating-point number.
+    def value(data)
+      BinData::DoubleLe.read(data)
+    end
+  end
+
+  # MySQL's Fixed-Point Type (DECIMAL), stored in InnoDB as a binary string.
+  class DecimalType
+    attr_reader :name, :width
+
+    # The value is stored as a sequence of signed big-endian integers, each
+    # representing up to 9 digits of the integral and fractional parts. The
+    # first integer of the integral part and/or the last integer of the
+    # fractional part might be compressed (or packed) and are of variable
+    # length. The remaining integers (if any) are uncompressed and 32 bits
+    # wide.
+    MAX_DIGITS_PER_INTEGER = 9
+    BYTES_PER_DIGIT = [0, 1, 1, 2, 2, 3, 3, 4, 4, 4]
+
+    def initialize(base_type, modifiers, properties)
+      precision, scale = sanity_check(modifiers)
+      integral = precision - scale
+      @uncomp_integral = integral / MAX_DIGITS_PER_INTEGER
+      @uncomp_fractional = scale / MAX_DIGITS_PER_INTEGER
+      @comp_integral = integral - (@uncomp_integral * MAX_DIGITS_PER_INTEGER)
+      @comp_fractional = scale - (@uncomp_fractional * MAX_DIGITS_PER_INTEGER)
+      @width = @uncomp_integral * 4 + BYTES_PER_DIGIT[@comp_integral] +
+               @comp_fractional * 4 + BYTES_PER_DIGIT[@comp_fractional]
+      @name = Innodb::DataType.make_name(base_type, modifiers, properties)
+    end
+
+    def value(data)
+      # Strings representing the integral and fractional parts.
+      intg, frac = "", ""
+
+      stream = StringIO.new(data)
+      mask = sign_mask(stream)
+
+      intg << get_digits(stream, mask, @comp_integral)
+
+      (1 .. @uncomp_integral).each do
+        intg << get_digits(stream, mask, MAX_DIGITS_PER_INTEGER)
+      end
+
+      (1 .. @uncomp_fractional).each do
+        frac << get_digits(stream, mask, MAX_DIGITS_PER_INTEGER)
+      end
+
+      frac << get_digits(stream, mask, @comp_fractional)
+
+      # Convert to something resembling a string representation.
+      str = mask.to_s.chop + intg + '.' + frac
+
+      BigDecimal.new(str).to_s('F')
+    end
+
+    private
+
+    # Ensure width specification (if any) is compliant.
+    def sanity_check(modifiers)
+      raise "Invalid width specification" unless modifiers.size <= 2
+      precision = modifiers.fetch(0, 10)
+      raise "Unsupported precision for DECIMAL type" unless
+        precision >= 1 and precision <= 65
+      scale = modifiers.fetch(1, 0)
+      raise "Unsupported scale for DECIMAL type" unless
+        scale >= 0 and scale <= 30 and scale <= precision
+      [precision, scale]
+    end
+
+    # The sign is encoded in the high bit of the first byte/digit. The byte
+    # might be part of a larger integer, so apply the bit-flipper and push
+    # back the byte into the stream.
+    def sign_mask(stream)
+      byte = BinData::Uint8.read(stream)
+      sign = byte & 0x80
+      byte.assign(byte ^ 0x80)
+      stream.rewind
+      byte.write(stream)
+      stream.rewind
+      (sign == 0) ? -1 : 0
+    end
+
+    # Return a string representing an integer with a specific number of digits.
+    def get_digits(stream, mask, digits)
+      nbits = BYTES_PER_DIGIT[digits] * 8
+      return "" unless nbits > 0
+      value = (BinData::const_get("Int%dbe" % nbits).read(stream) ^ mask)
+      # Preserve leading zeros.
+      ("%0" + digits.to_s + "d") % value
     end
   end
 
@@ -71,12 +207,19 @@ class Innodb::DataType
 
   # Maps base type to data type class.
   TYPES = {
+    :BIT        => BitType,
+    :BOOL       => IntegerType,
+    :BOOLEAN    => IntegerType,
     :TINYINT    => IntegerType,
     :SMALLINT   => IntegerType,
     :MEDIUMINT  => IntegerType,
     :INT        => IntegerType,
     :INT6       => IntegerType,
     :BIGINT     => IntegerType,
+    :FLOAT      => FloatType,
+    :DOUBLE     => DoubleType,
+    :DECIMAL    => DecimalType,
+    :NUMERIC    => DecimalType,
     :CHAR       => CharacterType,
     :VARCHAR    => VariableCharacterType,
     :BLOB       => BlobType,
