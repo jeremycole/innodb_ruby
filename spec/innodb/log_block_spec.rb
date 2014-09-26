ec1dc73d00d6ef58ad93208a71d504b45c9591a2
Jeremy Cole <jeremy@jcole.us>
2014-09-25 18:23:26 -0700

    Update deprecated rspec 'its' syntax tests

--- spec/innodb/log_block_spec.rb
+++ spec/innodb/log_block_spec.rb
@@ -3,19 +3,27 @@ require 'spec_helper'
 
 describe Innodb::LogBlock do
   before :all do
-    @block = Innodb::Log.new("spec/data/ib_logfile0").block(0)
+    @log = Innodb::Log.new("spec/data/ib_logfile0")
+    @block = @log.block(0)
   end
 
-  subject { @block }
+  describe "#block" do
+    it "has a correct checksum" do
+      @block.checksum.should eql 1706444976
+    end
 
-  its(:checksum) { should eql 1706444976 }
-  its(:corrupt?) { should eql false }
-  its(:header) do
-    should eql(
-      :flush            => true,
-      :block_number     => 17,
-      :data_length      => 512,
-      :first_rec_group  => 12,
-      :checkpoint_no    => 5)
+    it "is not corrupt" do
+      @block.corrupt?.should eql false
+    end
+
+    it "returns a valid header" do
+      @block.header.should eql({
+        :flush            => true,
+        :block_number     => 17,
+        :data_length      => 512,
+        :first_rec_group  => 12,
+        :checkpoint_no    => 5,
+      })
+    end
   end
 end
