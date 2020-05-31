f0b705af91dd629f0caddea1800fadc5527fa461
Jeremy Cole <jeremy@jcole.us>
2020-05-30 17:46:50 -0700

    Use class instance var instead of class var for stats

--- lib/innodb/stats.rb
+++ lib/innodb/stats.rb
@@ -4,27 +4,27 @@
 # correctness checking.
 module Innodb
   class Stats
-    @@data = Hash.new(0)
+    @data = Hash.new(0)
 
-    # Return the data hash directly.
-    def self.data
-      @@data
+    class << self
+      attr_reader :data
     end
+
 
     # Increment a statistic by name (typically a symbol), optionally by a value
     # provided.
     def self.increment(name, value = 1)
-      @@data[name] += value
+      @data[name] += value
     end
 
     # Get a statistic by name.
     def self.get(name)
-      @@data[name]
+      @data[name]
     end
 
     # Reset all statistics.
     def self.reset
-      @@data.clear
+      @data.clear
       nil
     end
 
@@ -32,7 +32,7 @@ module Innodb
     # provided, or by default to STDOUT.
     def self.print_report(io = STDOUT)
       io.puts '%-50s%10s' % %w[Statistic Count]
-      @@data.sort.each do |name, count|
+      @data.sort.each do |name, count|
         io.puts '%-50s%10i' % [
           name,
           count,
